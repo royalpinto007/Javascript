2e85721fef4f2ead7670b9249604c57c05541f34
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-13 00:47:41 +0530

    Update and rename 4 Add New Properties to a Object.js to 4 Add & Delete New Properties to a Object.js

---
+++ Basic Javascript/Objects/4 Add & Delete New Properties to a Object.js
@@ -0,0 +1,14 @@
+/*
+Add properties to an Object
+
+ourDog.bark = "bow-wow";        // using dot notation
+ourDog["bark"] = "bow-wow";     // using bracket notation
+
+This adds bark property to ourDog.
+__________________________________________________________
+Delete Properties from an Object
+
+delete ourDog.bark;
+
+This deletes the bark property from ourDog.
+*/
--- Basic Javascript/Objects/4 Add New Properties to a Object.js
+++
@@ -1,6 +0,0 @@
-/*
-ourDog.bark = "bow-wow";        // using dot notation
-ourDog["bark"] = "bow-wow";     // using bracket notation
-
-This adds bark property to ourDog.
-*/
