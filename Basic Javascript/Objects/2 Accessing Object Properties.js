46e9c9b1927c963e0c303be1bc6b17baf68fbb9c
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-13 00:18:06 +0530

    Update and rename Accessing Object Properties.js to 2 Accessing Object Properties.js

--- Basic Javascript/Objects/2 Accessing Object Properties.js
+++ Basic Javascript/Objects/2 Accessing Object Properties.js
@@ -1,6 +1,6 @@
 /*
 There are two ways to access the properties of an object: dot notation (.) and bracket notation ([]), similar to an array.
-
+__________________________________________________________________________________________________________________________
 // Dot Notation
 Dot notation is what you use when you know the name of the property you're trying to access ahead of time.
 
