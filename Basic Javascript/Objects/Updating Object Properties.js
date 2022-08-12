8b6c4249adf68b8087940915655ff2cb113fae4f
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-13 00:32:00 +0530

    Create Updating Object Properties.js

---
+++ Basic Javascript/Objects/Updating Object Properties.js
@@ -0,0 +1,12 @@
+/*
+const ourDog = 
+{
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";    // using dot notation
+ourDog["name"] = "Happy Camper";  // using bracket notation
+*/
